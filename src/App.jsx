ec7241a68125da8be805497b3991412d6acd6c54
ChrisAvellar <2kavellargmail.com>
2024-11-02 19:56:16 -0300

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import Header from './components/Header'
+import AppRoutes from './routes/AppRoutes'
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+      <Header/>
+      <AppRoutes/>      
+      </BrowserRouter>
+    </div>
   )
 }
